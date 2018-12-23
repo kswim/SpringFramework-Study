70fc223558296a0831d5d39bb31b3bee7777a505
kswim <xoxo900@naver.com>
2018-12-23 16:30:57 +0900

    Create an Apache Tomcat server and Web project without STS

---
+++ Lec16/src/main/java/com/lec/lec16/HomeController.java
@@ -0,0 +1,23 @@
+package com.lec.lec16;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home(Model model) {
+		System.out.println("----- home() method -----");
+		model.addAttribute("key", "home value");
+		return "home";
+	}
+	
+	@RequestMapping("/login")
+	public String login(Model model) {
+		System.out.println("----- login() method -----");
+		model.addAttribute("key", "login value");
+		return "login";
+	}
+}
